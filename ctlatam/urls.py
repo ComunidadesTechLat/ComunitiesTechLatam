c739f15725ff7c7877dc499636ffa65e454f4b86
Alejandro Medina <5almero5@gmail.com>
2020-07-28 14:37:16 -0500

    Add templates more urls

--- ctlatam/urls.py
+++ ctlatam/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path
 from  web import views as get_views
 urlpatterns = [
-    path('', get_views.home),
-    path('about/', get_views.about),
-    path('community/<int:id>', get_views.community),
+    path('', get_views.home, name='home'),
+    path('about/', get_views.about, name='about'),
+    path('events/', get_views.events, name='events'),
+    path('contact/', get_views.contact, name='contact'),
+    path('community/<uuid:id>', get_views.community, name='community'),
     path('admin/', admin.site.urls),
 ]
