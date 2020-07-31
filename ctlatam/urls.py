9b13101c2e26f2be7fe0ce44f9f60e0496536cbd
Lalo Rivero <eduardo.rivero@cobachih.edu.mx>
2020-07-30 20:43:44 -0600

    new community url

--- ctlatam/urls.py
+++ ctlatam/urls.py
@@ -1,21 +1,9 @@
-"""ctlatam URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from  web import views as get_views
+from communities import views as communities_views
+
+
 urlpatterns = [
     path('', get_views.home, name='home'),
     path('about/', get_views.about, name='about'),
@@ -23,4 +11,7 @@ urlpatterns = [
     path('contact/', get_views.contact, name='contact'),
     path('community/<uuid:id>', get_views.community, name='community'),
     path('admin/', admin.site.urls),
+    
+    # Communities
+    path('community/new/', communities_views.add_new_community, name='add_new_community'),
 ]
