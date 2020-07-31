9008ecb02f9a2cee56b3714ca35b05b246a52b67
Lalo Rivero <eduardo.rivero@cobachih.edu.mx>
2020-07-30 22:27:15 -0600

    fixing models.py pylint errors

--- communities/models.py
+++ communities/models.py
@@ -1,40 +1,39 @@
 """ Community Model """
 
-from django.db import models
 import uuid
+from django.db import models
 
 class Community(models.Model):
     """ Community Model """
- 
     #Display
-    display=models.BooleanField(default=False)
-    id=models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    display = models.BooleanField(default=False)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
 
     # Main Info
-    name=models.CharField(max_length=255, unique=True)
-    logo=models.ImageField(upload_to='communities/logos', blank=True, null=True)
-    img=models.ImageField(upload_to='communities/imgs', null=True, blank=True)
-    description=models.TextField()
-    quantity_of_members=models.IntegerField(blank=True, null=True)
+    name = models.CharField(max_length=255, unique=True)
+    logo = models.ImageField(upload_to='communities/logos', blank=True, null=True)
+    img = models.ImageField(upload_to='communities/imgs', null=True, blank=True)
+    description = models.TextField()
+    quantity_of_members = models.IntegerField(blank=True, null=True)
 
 
     #Contact
-    web=models.CharField(max_length=255, blank=True, null=True)
-    email=models.EmailField(max_length=255, blank = True, null=True)
-    fb_page=models.URLField(blank = True, null=True)
-    twitter=models.URLField(blank = True, null=True)
-    instagram=models.URLField(blank = True, null=True)
-    github=models.URLField(blank = True, null=True)
+    web = models.CharField(max_length=255, blank=True, null=True)
+    email = models.EmailField(max_length=255, blank=True, null=True)
+    fb_page = models.URLField(blank=True, null=True)
+    twitter = models.URLField(blank=True, null=True)
+    instagram = models.URLField(blank=True, null=True)
+    github = models.URLField(blank=True, null=True)
 
 
     #Location
-    country=models.CharField(max_length=50)
-    city=models.CharField(max_length=50, blank=True, null=True)
+    country = models.CharField(max_length=50)
+    city = models.CharField(max_length=50, blank=True, null=True)
 
 
     # Category
-    category=models.ForeignKey(
+    category = models.ForeignKey(
         to="Category",
         on_delete=models.SET_NULL,
         null=True
@@ -43,7 +42,7 @@ class Community(models.Model):
 
     #Flags
 
-    flags=models.ManyToManyField(
+    flags = models.ManyToManyField(
         to='Flag'
     )
 
@@ -55,7 +54,7 @@ class Community(models.Model):
         ('Revision', 'In revision')
     ]
 
-    status=models.CharField(
+    status = models.CharField(
         choices=STATUS_CHOICES,
         max_length=15,
         null=False,
@@ -65,69 +64,63 @@ class Community(models.Model):
 
     # DC Counters
 
-    interactions=models.IntegerField(default=0)
-    reads=models.IntegerField(default=0)
+    interactions = models.IntegerField(default=0)
+    reads = models.IntegerField(default=0)
 
 
     # DB info
 
-    created=models.DateTimeField(auto_now=True)
-    modified=models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now=True)
+    modified = models.DateTimeField(auto_now=True)
 
-    def __str__(self):
-        return self.name
 
 
 class Category(models.Model):
     """ Lists the avaliable categories  """
     class Meta:
         # pylint: disable=C0115,R0903
-        verbose_name_plural="Categories"
+        verbose_name_plural = "Categories"
 
-    name=models.CharField(max_length=60)
+    name = models.CharField(max_length=60)
 
-    def __str__(self):
-        return self.name
 
 
 class Country(models.Model):
     """ Lists the avaliable countries """
     class Meta:
         # pylint: disable=C0115,R0903
-        verbose_name_plural="Countries"
+        verbose_name_plural = "Countries"
 
     LATAM_ISO_COUNTRIES = [
-        ('BZ','Belice'),
-        ('CR','Costa Rica'),
-        ('SV','El Salvador'),
-        ('GT','Guatemala'),
-        ('HN','Honduras'),
-        ('NI','Nicaragua'),
-        ('PA','Panamá'),
-        ('AR','Argentina'),
-        ('BO','Bolivia'),
-        ('BR','Brasil'),
-        ('CL','Chile'),
-        ('CO','Colombia'),
-        ('EC','Ecuador'),
-        ('GY','Guyana'),
-        ('GF','Guyana Francesa'),
-        ('PY','Paraguay'),
-        ('PE','Perú'),
-        ('SR','Suriname'),
-        ('UY','Uruguay'),
-        ('VE','Venezuela'),
-        ('MX','México'),
+        ('BZ', 'Belice'),
+        ('CR', 'Costa Rica'),
+        ('SV', 'El Salvador'),
+        ('GT', 'Guatemala'),
+        ('HN', 'Honduras'),
+        ('NI', 'Nicaragua'),
+        ('PA', 'Panamá'),
+        ('AR', 'Argentina'),
+        ('BO', 'Bolivia'),
+        ('BR', 'Brasil'),
+        ('CL', 'Chile'),
+        ('CO', 'Colombia'),
+        ('EC', 'Ecuador'),
+        ('GY', 'Guyana'),
+        ('GF', 'Guyana Francesa'),
+        ('PY', 'Paraguay'),
+        ('PE', 'Perú'),
+        ('SR', 'Suriname'),
+        ('UY', 'Uruguay'),
+        ('VE', 'Venezuela'),
+        ('MX', 'México'),
     ]
-    
+
     name = models.CharField(
         choices=LATAM_ISO_COUNTRIES,
         max_length=60,
-        null=False,    
+        null=False,
     )
 
-    def __str__(self):
-        return self.name
 
 
 class Location(models.Model):
@@ -142,19 +135,6 @@ class Location(models.Model):
     latitude = models.FloatField(null=True)
     longitude = models.FloatField(null=True)
 
-    def __str__(self):
-        if self.latitude and self.longitude:
-            return "{0}'s location:\n Country:{1}\n coord:{2},{3}".format(
-                self.community,
-                self.country,
-                self.latitude,
-                self.longitude
-            )
-
-        return "{0}'s location:\n Country:{1}".format(
-            self.community,
-            self.country
-        )
 
 
 class Flag(models.Model):
@@ -164,6 +144,3 @@ class Flag(models.Model):
 
     name = models.CharField(max_length=20)
     icon = models.CharField(max_length=3)
-
-    def __str__(self):
-        return self.name
