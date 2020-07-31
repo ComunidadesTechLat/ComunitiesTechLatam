8f3a82d562b4d829821dc66e1feadbc2dd763434
Lalo Rivero <eduardo.rivero@cobachih.edu.mx>
2020-07-30 23:00:58 -0600

    Adding models to admin

--- communities/admin.py
+++ communities/admin.py
@@ -3,7 +3,11 @@
 from django.contrib import admin
 
 # Models
-from communities.models import Community
+from communities.models import Community, Category, Country, Location, Flag
 
 # Register your models here.
 admin.site.register(Community)
+admin.site.register(Category)
+admin.site.register(Country)
+admin.site.register(Location)
+admin.site.register(Flag)
