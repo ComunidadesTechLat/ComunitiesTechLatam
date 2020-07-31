7e6cee25ca1f5fab7bfc8dff9e5382fc81a9173e
Lalo Rivero <eduardo.rivero@cobachih.edu.mx>
2020-07-30 20:59:21 -0600

    community form

---
+++ communities/forms.py
@@ -0,0 +1,28 @@
+""" Community Forms """
+
+# Django
+from django import forms
+
+# Models
+from communities.models import Community
+
+class ComunityForm(forms.ModelForm):
+    """ Community model form """
+    
+    class Meta:
+        """ Form settings """
+        model = Community
+        fields = ('name', 
+                  'logo', 
+                  'description',
+                  'q_members',
+                  'web',
+                  'email',
+                  'fb_page',
+                  'twitter',
+                  'instagram',
+                  'github',
+                  'country',
+                  'city',
+                  'category'
+                  )
