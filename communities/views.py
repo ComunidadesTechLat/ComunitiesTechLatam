aad3f87f751172f2d7a924105157ec2633c6bb23
Lalo Rivero <eduardo.rivero@cobachih.edu.mx>
2020-07-30 21:00:45 -0600

    community view

---
+++ communities/views.py
@@ -0,0 +1,15 @@
+#Django
+from django.shortcuts import render, redirect
+from django.views.generic import FormView
+from django.urls import reverse_lazy
+
+# Froms
+from communities.forms import Community
+
+# Create your views here.
+class add_new_community(FormView):
+    """ Add a new community """
+
+    template_name = 'community/new.html'
+    form_class = Community
+    success_url = reverse_lazy('/')
