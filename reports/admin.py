d36735be2a5e66a4c55e9ed80ee1ce7450ab84c5
Lalo Rivero <eduardo.rivero@cobachih.edu.mx>
2020-08-01 14:33:50 -0600

    fixing pylint errors

--- reports/admin.py
+++ reports/admin.py
@@ -1,3 +1,20 @@
+""" User Reports admin cofig """
+
+# Django
 from django.contrib import admin
 
-# Register your models here.
+# Models
+from reports.models import ReportModel
+
+@admin.register(ReportModel)
+class ReportAdmin(admin.ModelAdmin):
+    """ Reports Admin """
+
+    list_display = ('status', 'community_name', 'subject', 'description')
+    list_display_links = ('communiy_name')
+    list_editable = ('status')
+
+    search_fields = (
+        'status',
+        'community_name'
+    )
