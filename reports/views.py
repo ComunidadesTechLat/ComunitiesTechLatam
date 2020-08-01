880388c7cabb67d9d6153349354c37096e9dd74c
Lalo Rivero <eduardo.rivero@cobachih.edu.mx>
2020-08-01 14:34:13 -0600

    Reports view

--- reports/views.py
+++ reports/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+""" Reports View """
 
-# Create your views here.
+#Django
+from django.views.generic import FormView
+from django.urls import reverse_lazy
+
+# Froms
+from reports.forms import UserReportForm
+
+
+class UserReport(FormView):
+    """ Report a community """
+
+    template_name = 'report.html'
+    form_class = UserReportForm
+    success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        """ Save a new report """
+        form.save()
+        return super().form_valid(form)
