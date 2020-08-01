1ef6eedb0c527d164e50401525ac9ceaa61a9da6
Lalo Rivero <eduardo.rivero@cobachih.edu.mx>
2020-08-01 13:14:27 -0600

    Display info for communities in DjangoAdmin

--- communities/admin.py
+++ communities/admin.py
@@ -5,7 +5,29 @@ from django.contrib import admin
 # Models
 from communities.models import Community, Category, Flag
 
-# Register your models here.
-admin.site.register(Community)
-admin.site.register(Category)
-admin.site.register(Flag)
+
+@admin.register(Community)
+class CommunityAdmin(admin.ModelAdmin):
+    """ Community Admin """
+
+    list_display = ('status', 'name', 'web', 'email', 'facebook_page', 'twitter', 'instagram', 'github', 'country')
+    list_display_links = ('name',)
+    list_editable = ('status', )
+
+    search_fields = (
+        'status',
+        'name',
+        'country',
+    )
+
+    list_filter = ('status', 'created', 'modified')
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Flag)
+class FlagAdmin(admin.ModelAdmin):
+    pass
