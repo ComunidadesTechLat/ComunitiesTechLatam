5bb9dfcd37f543ada61cdda761bff8095cf6200a
Lalo Rivero <eduardo.rivero@cobachih.edu.mx>
2020-07-31 19:36:23 -0600

    Typo name

--- communities/views.py
+++ communities/views.py
@@ -4,12 +4,12 @@ from django.views.generic import FormView
 from django.urls import reverse_lazy
 
 # Froms
-from communities.forms import Community
+from communities.forms import ComunityForm
 
 # Create your views here.
 class AddNewCommunity(FormView): # pylint: disable=too-many-ancestors
     """ Add a new community """
 
     template_name = 'community/new.html'
-    form_class = Community
+    form_class = ComunityForm
     success_url = reverse_lazy('/')
