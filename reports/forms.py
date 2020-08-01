3161caf1b1ffdec2f1a7105ee33a778e5765f7c9
Lalo Rivero <eduardo.rivero@cobachih.edu.mx>
2020-08-01 14:15:11 -0600

    reports form

---
+++ reports/forms.py
@@ -0,0 +1,18 @@
+""" Report Form """
+
+# Django
+from django import forms
+
+# Models
+from reports.models import ReportModel
+
+class UserReport(forms.ModelForm):
+    """ User Reports model form """
+
+    class Meta:
+        """ Form settings """
+        model = ReportModel
+        fields = ('communiy_name',
+                  'subject',
+                  'description',
+                  )
